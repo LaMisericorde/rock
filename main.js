7673298f13135dcb731d7adf31548b5bd0c82d1d
Friedrich Erbach <friedrich.erbach@protonmail.com>
2022-05-01 15:03:03 +0300

    Finished the game, playable only in console

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let invalidSelection = 0;
+let round = 1;
+let playerWins = 0;
+let computerWins = 0;
 
 function computerPlay() {
     const computerSelection = Math.floor(Math.random() * 3 );
@@ -38,20 +41,38 @@ function singleRound() {
     const playerSelection = humanPlay();
     const computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
+        round--;
         return "Tie";
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        computerWins++;
         return "You lose! Paper beats Rock!";
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        playerWins++;
         return "You win! Rock beats Scissors!";
     } else if (playerSelection == "PAPER" && computerSelection === "ROCK") {
+        playerWins++;
         return "You win! Paper beats Rock!";
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        computerWins++;
         return "You lose! Scissors beat Paper!";
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        computerWins++;
         return "You lose! Rock beats Scissors!";
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        playerWins++;
         return "You win! Scissors beat Paper!";
     }
 }
 
-console.log(singleRound());
+function game() {
+    for (round = 1; round < 6; round++) {
+        console.log(singleRound());
+        if (playerWins === 3) {
+            return "You won the game!";
+        } else if (computerWins === 3) {
+            return "You lost the game!";
+        }
+    }
+}
+
+console.log(game());
